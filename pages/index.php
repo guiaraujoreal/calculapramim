fa04bebe0ba9475691afef1815d655aba67bd73f
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2023-04-12 14:17:25 -0300

    Criacao

---
+++ pages/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt1">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="../style/index.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../style/cabecalho-rodape.css" media="screen" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+
+    <title>Bem-vindo ao Calcula Pra Mim</title>
+</head>
+<body>
+    <main>
+        <div id="cabecalho">
+            <p class="titulo">CALCULA PRA MIM</p>
+            <div id="menu">
+                <div id="menu-bar" onclick="menuOnClick()">
+                    <div id="bar1" class="bar"></div>
+                    <div id="bar2" class="bar"></div>
+                    <div id="bar3" class="bar"></div>
+                </div>
+            <nav class="nav" id="nav">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+            </nav> 
+            </div>
+
+            <div class="menu-bg" id="menu-bg"></div>
+        </div>
+
+
+        <footer>
+        </footer>
+    </main>
+    <script src="../js/functions.js"></script>
+</body>
+</html>
