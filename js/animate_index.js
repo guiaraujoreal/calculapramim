6eeb3129758a09c0c7c42bbe0a7022d0c6cfc066
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2023-04-14 19:08:14 -0300

    cor

--- js/animate_index.js
+++ js/animate_index.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded',() =>{
     new TypeIt(".animated", {
         speed:200,
         loop: true
-    }).type('Potenciação?',{delay:900}).delete(12).type('Radiciação?',{delay:900}).delete(11).type('Notação Cietífica?',{delay:900}).delete(17)
+    }).type('Potenciação?',{delay:900}).delete(12).type('Radiciação?',{delay:900}).delete(11).type('Notação Científica?',{delay:900}).delete(18)
     .go()
 })
