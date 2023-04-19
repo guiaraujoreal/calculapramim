02f1d958eac937c36e4b8d56f6c0e6d02056acf7
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2023-04-19 17:33:25 -0300

    reajuste bot-page

--- api/bot.php
+++ api/bot.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../../style/bot.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="../../style/cabecalho-rodape.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../style/bot.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="../style/cabecalho-rodape.css" media="screen" />
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -22,14 +22,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <script src="https://unpkg.com/typeit@8.7.1/dist/index.umd.js" crossorigin="anonymous"></script>
 
-    <link rel="shortcut icon" href="../../imgs/ico.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../imgs/ico.png" type="image/x-icon">
     <title>Prazer! Sou o Math!</title>
 </head>
 <body>
 <header>
         <div id="cabecalho" class="container container-fluid">
             <div id="col_logo" class="row d-flex align-items-center">
-                <div><img id="logo" class="col" src="../../imgs/logo.png" alt="logotipo"></div>
+                <div><img id="logo" class="col" src="../imgs/logo.png" alt="logotipo"></div>
                 <div id="menu">
             <div id="menu-bar" onclick="menuOnClick()">
                 <div id="bar1" class="bar"></div>
@@ -59,6 +59,13 @@
     </header>
 
     <main>
+    <div id="container02" class="container container-fluid">
+        <div class="row">
+            <div class="col">
+            <p id="texto_cont02"><b>Pergunte algo ao Math👇</b></p>
+            </div>
+        </div>
+    </div>
     <div id="container01" class="container container-fluid">
             <div class="row justify-content-around">
                 <div id="col_subtitle" class="col "><b><span id="sub_title" class="animated"></span></b></div>
@@ -69,20 +76,14 @@
             </div>
         </div>
     </div>
-    <div id="container02" class="container container-fluid">
-        <div class="row">
-            <div class="col">
-            <p id="texto_cont02"><b>Pergunte algo ao Math👇</b></p>
-            </div>
-        </div>
-    </div>
+    
     </main>
 
     <footer id="rodape" class="container">
         <p id="copy">&copy; 2023 Meu Site</p>
     </footer>
 
-    <script src="../../js/animate_subtitle_bot.js"></script>
+    <script src="../js/animate_subtitle_bot.js"></script>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
