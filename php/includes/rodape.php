6c280a5d043d7f6079bc2feb4a7996161ae06c64
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2023-05-12 17:31:03 -0300

    footer

--- php/includes/rodape.php
+++ php/includes/rodape.php
@@ -9,15 +9,17 @@
     <div class="row">
         <div class="col"><img id="logo_footer" src="../../imgs/logo.png" alt="logotipo"></div>
     </div>
-    <div class="row">
+    
+    <div class="row copy_bot">
             <div class="col_copy col d-flex justify-content-start">
                 <p class="copy"><tag class="dev_tag">&copy;2023 Copyright - Todos o direitos reservados | Desenvolvido por </tag> João Guilherme</p>
             </div>
-            <div 
-            <div class="col_icons col d-flex justify-content-around">
-                    <img class="icons_rs col" src="../../imgs/instagram.png">
-                    <img class="icons_rs col" src="../../imgs/linkedin.png">
-                    <img class="icons_rs col" src="../../imgs/whatsapp.png">
+            <div class="col_icons col d-flex justify-content-end align-items-center">
+                <div class="row">
+                    <img class="icons_rs" src="../../imgs/instagram.png">
+                    <img class="icons_rs" src="../../imgs/linkedin.png">
+                    <img class="icons_rs" src="../../imgs/whatsapp.png">
+                </div>
             </div>
     </div>
 </div>
