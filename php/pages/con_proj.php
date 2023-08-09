a6f466c68091c78657f6f3cc4bd6d43854f88ee1
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2023-08-09 14:10:34 -0300

    add alternar tema con_proj

--- php/pages/con_proj.php
+++ php/pages/con_proj.php
@@ -37,7 +37,7 @@
     <main>
       <section class="container-fluid d-flex justify-content-center flex-wrap" style="padding-top: 200px; padding-bottom: 10vw">
         <div class="col-12 d-flex justify-content-center">
-          <img src="../../imgs/4040_error.svg" style="height: auto; width: 400px;">
+          <img src="../../imgs/4040_error.svg" style="height: auto; width: 300px;">
         </div>
         <div class="col d-flex d-flex justify-content-center">
           <p style="font-size: 25px; text-align: center" >Ops! Ainda estamos construindo essa página.</p>
@@ -49,13 +49,27 @@
       <?php include('../includes/rodape.php'); ?>
     </footer>
 
+      <script src="../../assets/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="../../js/menu_responsivo.js"></script>
+      <script src="../../js/functions.js"></script>
 
 
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-
+      <script>
+      function alternarTema() {
+      var estiloTema = document.getElementById("estilo-tema");
+      if (estiloTema.getAttribute("href") === "../../style/themes/tema-escuro.css") {
+        estiloTema.setAttribute("href", "../../style/themes/tema-claro.css");
+      } else {
+        estiloTema.setAttribute("href", "../../style/themes/tema-escuro.css");
+      }
+        
+      document.body.classList.remove("../../style/themes/tema-claro.css"); // Remova a classe "dark-theme" do body para retornar ao tema claro
+    }
+    </script>
 
 
             
