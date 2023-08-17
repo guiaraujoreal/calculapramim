5a7ab67da2f6a9376fb984b06516013eb593a31d
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2023-08-17 19:48:18 -0300

    reface animation AM

--- php/includes/overlayContAM.php
+++ php/includes/overlayContAM.php
@@ -1,7 +1,7 @@
 <div id="overlay" class="container-fluid">
       <div class="row body_load d-flex justify-content-center">
         <div id="animate_over" class="row d-flex justify-content-around justify-content-center">
-        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script><lottie-player id="animate_over" src="https://lottie.host/693763cd-dd8a-4bc4-a9a3-34d135ea1ca6/qJsId86AgE.json" background="#FFFFFF00" speed="1" direction="1" mode="normal" loop autoplay></lottie-player>
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script><lottie-player id="animateAM" src="https://lottie.host/693763cd-dd8a-4bc4-a9a3-34d135ea1ca6/qJsId86AgE.json" background="#FFFFFF00" speed="1" direction="1" mode="normal" loop autoplay></lottie-player>
         </div>
         <div class="row row2 w-100 d-flex justify-content-around justify-content-center">
             <b><span class="text_load d-flex justify-content-center" style="color: white"></span></b>
