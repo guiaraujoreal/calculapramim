e7947133fe3f7b2887a12ea0d7374664d5be5263
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2023-08-20 20:31:27 -0300

    correcao vercel

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ const port = 3000;
 const waApi = WolframAlphaAPI('R73LK6-787HQYGX78');
 
 app.use(express.static(__dirname));
-
-app.use(express.static('style', {
-    setHeaders: (res, path) => {
-        if (path.endsWith('.css')) {
-            res.setHeader('Content-Type', 'text/css');
-        }
-    }
-}));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/node/index.html');
+});
+app.get('/big', (req, res) => {
+    //versao de dev
+    
+    // res.redirect('http://localhost:80/sites/math-learn/php/pages/index.php');
+    //versao de producao
+    res.redirect('https://www.mathlearn.com.br');
+    
+});
 
 app.use(bodyParser.json());
 
